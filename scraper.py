32343bdd08d5356ab98ab48c39b4a389c96a5baf
chloechan <chanco@uci.edu>
2025-04-25 00:47:55 -0700

    created the helper function which includes most of the requirements from canvas

--- scraper.py
+++ scraper.py
@@ -3,8 +3,6 @@ from urllib.parse import *
 from bs4 import BeautifulSoup
 from collections import Counter
 
-
-#i took from the is_valid function and put the list of skipped words in a list, might reuse to check other functions later
 skipped_words = [".css", ".js", ".bmp", ".gif", ".jpg", ".jpeg", ".ico",
     ".png", ".tif", ".tiff", ".mid", ".mp2", ".mp3", ".mp4",
     ".wav", ".avi", ".mov", ".mpeg", ".ram", ".m4v", ".mkv", ".ogg", ".ogv", ".pdf",
@@ -35,26 +33,15 @@ def extract_next_links(url, resp):
         print(f"Error extracting links: {e}")
     
     return links
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
     
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        #above is given
+        
         #check if the domain is uci.edu or uci.edu, check if it's ics, cs, informatics, or stat
         # and check if the path does not end with any of the skipped words
         if not re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu.*", parsed.netloc):
@@ -67,33 +54,20 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for URL", parsed)
         return False
-        # raise
 
+def crawl_pages_checker(url, resp):
+    """
+    Check if the page is a valid page to crawl. Call it in the scraper? This is based on the behavior 
+    requirements on the instruction page on canvas
+    """
+    if trap_detected(url):
+        pass
 
-            # return not re.match(
-        #     r".*\.(css|js|bmp|gif|jpe?g|ico"
-        #     + r"|png|tiff?|mid|mp2|mp3|mp4"
-        #     + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-        #     + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-        #     + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-        #     + r"|epub|dll|cnf|tgz|sha1"
-        #     + r"|thmx|mso|arff|rtf|jar|csv"
-        #     + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+    if bad_url(resp):
+        pass
+    
+    if high_info_content(resp):
+        pass
 
-#below are some potential functions I think we can implement?
-
-def bad_context_url(url, resp):
-    pass
-
-def duplicate_url(url, resp):
-    pass
-
-def get_links(url, resp):
-    pass
-
-
-def update_links_content(url, resp):
-    pass
-
-def save_content_to_text():
-    pass
+    if similar_no_info(url, resp.raw_response):
+        pass
