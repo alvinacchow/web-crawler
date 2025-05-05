5df3e53cc455713b7502ba9cbe9fe53374c41e18
Pauline Kang <paulinjk@uci.edu>
2025-05-05 16:00:06 -0700

    modify is_valid for when checking for urls to crawl

--- scraper.py
+++ scraper.py
@@ -105,10 +105,17 @@ def is_valid(url):
         
         #check if the domain is uci.edu or uci.edu, check if it's ics, cs, informatics, or stat
         # and check if the path does not end with any of the skipped words
+        '''
         if parsed.netloc == "today.uci.edu" and parsed.path.startswith("/department/information_computer_sciences"):
-            return True
+            pass
         elif not re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu.*", parsed.netloc):
             return False
+        '''
+
+        if not ((parsed.netloc == "today.uci.edu" and parsed.path.startswith("/department/information_computer_sciences"))
+            or re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu.*", parsed.netloc)):
+        return False
+
 
         if any(parsed.path.lower().endswith(extension) for extension in skipped_words):
             return False
